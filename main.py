548dd720839d0dc794132cf46707136403b3f5b3
Shreyas Patil <shreyaspatil@Shreyass-MacBook-Air.local>
2026-05-19 20:35:41 +0530

    Added CORS support

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ app = FastAPI(
     description="Spotify Wrapped for Developers",
     version="1.0.0"
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(
     CORSMiddleware,
